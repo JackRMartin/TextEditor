87e893e05c12caeb885dabb16eb40ce2e45d15c4
JackRMartin <jackmartin@csu.fullerton.edu>
2018-07-11 19:51:23 -0700

    Add project files.

---
+++ TextEditor/main.cpp
@@ -0,0 +1,38 @@
+#include <Windows.h>
+#include <string>
+
+// create window
+// allow window to be resizable and allow user to type inside of the window.
+// create a toolbar in the window for saving the current file.
+const char g_sz_class_name[] = "myWindowClass";
+
+int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance,
+	LPSTR lp_cmd_line, int n_cmd_show)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG msg;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = h_instance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_sz_class_name;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+
+
+	return 0;
+}
